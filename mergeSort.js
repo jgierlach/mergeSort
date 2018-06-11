532fdd41839a489f99bde40439abd4fcfcbb17d6
jgierlach <jegierlach@gmail.com>
2018-06-11 14:35:53 -0500

    Jan's next commit

--- mergeSort.js
+++ mergeSort.js
@@ -9,8 +9,29 @@ function split(wholeArray) {
       return [firstHalf, secondHalf];
     }
 
-    console.log(split([1, 2, 3, 4, 5]))
+    //console.log(split([1, 2, 3, 4, 5]))
 
+function merge(arr1, arr2) {
+    return arr1.concat(arr2)
+}
+
+//console.log(merge([1,2,3], [4,5,6]))
+
+function mergeSort(arr) {
+    console.log(arr)
+    if(arr.length === 1) {
+
+    } 
+    else {
+        split(arr).forEach((index) => {
+            mergeSort(index)
+        })
+    }
     
+}
+
+mergeSort([2, 1])
+//output: [[3], [5], [7], [2], [5], [8]]
+
 
     
--- mergeSort.spec.js
+++ mergeSort.spec.js
@@ -10,3 +10,21 @@ describe('Split Array function', function() {
     });
     
   });
+
+  describe('Merge function', function() {
+ 
+      it('is able to merge to sorted arrays into one sorted array', function() {
+          expect(merge([1,2,3], [4,5,6])).toEqual([1,2,3,4,5,6])
+      })
+  })
+
+  describe('Merge sort', function(){
+      let arr;
+      beforeEach(() => {
+        arr = [5, 3, 2, 7, 8, 11, 55, -7]
+      });
+
+      it('check to see if the merge sort function is called', function() {
+        expect(split(arr)).toHaveBeenCalled()
+      })
+  })
